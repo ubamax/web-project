fc4b704b8d1ff25b3e7e0eacdca5f9c40d794f76
unknown <peterayuba75@yahoo.com>
2022-09-19 09:40:09 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -12,3 +12,15 @@ window.onscroll = () =>{
 };
 
 
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor: true,
+    loop:true,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
+
